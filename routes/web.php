15a8a300663cd58a2c8ab4ab17d12153518c3ef7
DungHoang0411 <dung042006@gmail.com>
2026-07-07 10:15:12 +0700

    clean web.php

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,3 @@ Route::resource('job-posts', JobPostController::class);
 Route::get('/', function () {
     return redirect()->route('job-posts.index');
 });
-
-// Đăng ký toàn bộ route CRUD (index, create, store, show, edit, update, destroy)
-Route::resource('job-posts', JobPostController::class);
