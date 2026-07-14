cf2641339f115d4e82585ef8ff1d9d75bcdec8b4
DungHoang0411 <dung042006@gmail.com>
2026-07-14 13:49:35 +0700

    khởi tạo truy vấn

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -1,14 +1,21 @@
 <?php
+
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\JobPost;
 
 class Company extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name', 'slug', 'description'];
+    protected $guarded = [];
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
 
     public function jobPosts()
     {
--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -1,17 +1,24 @@
 <?php
+
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\JobPost;
 
 class Tag extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name', 'slug'];
+    protected $guarded = [];
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
 
     public function jobPosts()
     {
-        return $this->belongsToMany(JobPost::class, 'job_post_tag');
+        return $this->belongsToMany(JobPost::class, 'job_post_tag', 'tag_id', 'job_post_id');
     }
 }
